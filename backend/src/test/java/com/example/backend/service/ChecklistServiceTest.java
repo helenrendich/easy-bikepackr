ed536eb8c87144bc32076f95b94b6d4dbef3821c
helenrendich <helen.rendich@wannweil.de>
2023-03-17 16:32:50 +0100

    add class getAllChecklistsTest in ChecklistServiceTest

--- backend/src/test/java/com/example/backend/service/ChecklistServiceTest.java
+++ backend/src/test/java/com/example/backend/service/ChecklistServiceTest.java
@@ -1,5 +1,52 @@
 package com.example.backend.service;
 
+import com.example.backend.model.Checklist;
+import com.example.backend.repository.ChecklistRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
 class ChecklistServiceTest {
 
+    ChecklistRepository checklistRepository = mock(ChecklistRepository.class);
+    ChecklistService checklistService = new ChecklistService(checklistRepository);
+
+    Checklist testChecklist = new Checklist("testId", "testDestination", LocalDate.of(2024, 1, 8));
+    List<Checklist> expectedChecklists = List.of(testChecklist);
+    List<Checklist> expectedChecklistsEmpty = new ArrayList<>();
+
+    @Nested
+    @DisplayName("testing getAllChecklists()")
+    class getAllChecklistsTest {
+        @Test
+        @DisplayName("...returns all checklists if the repo is not empty")
+        void getAllChecklists_returnsAllChecklistsIfTheRepoIsNotEmpty() {
+            //GIVEN
+            when(checklistRepository.getAllChecklists()).thenReturn(expectedChecklists);
+            //WHEN
+            List<Checklist> actual = checklistService.getAllChecklists();
+            //THEN
+            verify(checklistRepository).getAllChecklists();
+            Assertions.assertEquals(expectedChecklists, actual);
+        }
+
+        @Test
+        @DisplayName("...returns an empty list if the repo is empty")
+        void getAllChecklistsEmptyRepo() {
+            //GIVEN
+            when(checklistRepository.getAllChecklists()).thenReturn(expectedChecklistsEmpty);
+            //WHEN
+            List<Checklist> actual = checklistService.getAllChecklists();
+            //THEN
+            verify(checklistRepository).getAllChecklists();
+            Assertions.assertEquals(expectedChecklistsEmpty, actual);
+        }
+    }
 }
