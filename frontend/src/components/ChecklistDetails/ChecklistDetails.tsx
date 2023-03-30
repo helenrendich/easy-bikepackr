d2a00e067f5da6f50a19a55c33b053878aebd90a
helenrendich <helen.rendich@wannweil.de>
2023-03-30 11:46:25 +0200

    implement edit button functionality

--- frontend/src/components/ChecklistDetails/ChecklistDetails.tsx
+++ frontend/src/components/ChecklistDetails/ChecklistDetails.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -7,7 +8,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Layout from "../Layout/Layout";
 import {useParams} from "react-router-dom";
 import {Checklist} from "../../models/Checklist";
-import {Button, Card, CardActions, CardContent} from "@mui/material";
+import {Box, Button, Card, CardActions, CardContent} from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 
 type ChecklistDetailsProps = {
@@ -16,28 +17,41 @@ type ChecklistDetailsProps = {
 }
 
 function ChecklistDetails(props: ChecklistDetailsProps) {
+    const [isUpdateVisible, setIsUpdateVisible] = useState(false);
+
     const {id} = useParams<{ id: string }>()
     const checklist = (!!id && (props.checklists.find((checklist: Checklist) => checklist.id === id) as Checklist)) || null
     const dateString = checklist?.startDate;
     const dateDate = dateString ? new Date(dateString) : null;
 
+    function handleUpdateClick() {
+        setIsUpdateVisible(true);
+    }
+
+    const headlineCard = (
+        <CardContent>
+            <Box display="flex" flexDirection="row" alignItems="flex-end">
+                <Typography gutterBottom variant="h5" component="div">
+                    {checklist?.destination}
+                </Typography>
+                <CardActions>
+                    <Button onClick={handleUpdateClick}>
+                        <EditIcon color="action"/>
+                    </Button>
+                </CardActions>
+            </Box>
+            <Typography variant="body2" color="text.secondary">
+                {dateDate?.toLocaleDateString("en-GB")}
+            </Typography>
+        </CardContent>
+    );
+
+
     return (
         <div>
             <Layout>
                 <Card>
-                    <CardContent>
-                        <CardActions sx={{justifyContent: "flex-end"}}>
-                            <Button>
-                                <EditIcon color="action"/>
-                            </Button>
-                        </CardActions>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {checklist?.destination}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {dateDate?.toLocaleDateString("en-GB")}
-                        </Typography>
-                    </CardContent>
+                    {isUpdateVisible ? (<CardContent></CardContent>) : headlineCard}
                 </Card>
                 <Typography gutterBottom variant="h6" margin={1}>Checklist</Typography>
                 <Accordion>
