fcd0bacef34ea9ee89526199623f9735b91c7eed
helenrendich <helen.rendich@wannweil.de>
2023-04-04 15:44:54 +0200

    add Item model and add items in Checklist model in frontend

--- frontend/src/models/Checklist.ts
+++ frontend/src/models/Checklist.ts
@@ -1,7 +1,10 @@
+import {Item} from "./Item";
+
 export type Checklist = {
     id: string
     destination: string
     startDate: string
+    items: Item[]
 }
 
 export type NewChecklist = Omit<Checklist, "id">
